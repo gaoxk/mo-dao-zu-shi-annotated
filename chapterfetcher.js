0c8e30b2e8d63998d8d0aa1784765a89055c2d58
gaoxk <gaoxuekristy@gmail.com>
2020-04-13 23:26:57 -0400

    add more chapter buttons

--- chapterfetcher.js
+++ chapterfetcher.js
@@ -1,7 +1,27 @@
-function loadCorretChapter () {
-    $("#chapterblob").load("./chapters/"+ $("#chapter option:selected").val() +".html");
+function getCurrentChapterNumber () {
+    return $("#chapter option:selected").val();
 }
 
-$("#chapter").change(loadCorretChapter);
+function loadChapter (num) {
+    $("#chapterblob").load("./chapters/chapter"+ num +".html");
+}
+
+function setChapter (num) {
+    $("#chapter select").val(num);
+}
+
+function loadLastChapter () {
+    const chapter = Math.max(parseint(getCurrentChapterNumber())-1, 1);
+    loadCorrectChapter(chapter);
+    setChapter(chapter);
+}
+
+function loadNextChapter () {
+    const chapter = Math.min(parseint(getCurrentChapterNumber())+1, 126);
+    loadCorrectChapter(chapter);
+    setChapter(chapter);
+}
+
+$("#chapter").change(loadChapter(getCurrentChapterNumber()));
    
-loadCorretChapter();
+loadChapter(getCurrentChapterNumber());
